c09444e76aa78e2034e0da4f9f9c64759e11d07d
Tom <tomtalp93@gmail.com>
2016-07-21 16:41:23 +0300

    Make the random date generator less random, and give lunch-time hours a higher probability

--- data-generator.py
+++ data-generator.py
@@ -3,9 +3,53 @@ import json
 import datetime
 import random
 import uuid
-# data = {"mac_addr": "blabla2","first_seen": str(datetime.datetime.now()),"last_seen": str(datetime.datetime.now()),"insertion_time": str(datetime.datetime.now())}
 
-# client.put_event(stream=stream, data=json.dumps(data), method="post")
+def get_hour_weight(hour):
+    """
+    Give every hour range a differetn weight, so that we can replicate real
+    customer data more accurately
+    """
+    if hour <= 8 or hour >= 22:
+        return 5
+    elif hour > 8 and hour <= 11:
+        return 25
+    elif hour > 11 and hour <= 14:
+        return 100
+    elif hour > 14 and hour <= 18:
+        return 30
+    else:
+        return 15
+
+def get_random_hour():
+    """
+    Generate a random hour, by our "hours-weight list". With the weight list we
+    build a list of hours, in which the popular hours appear more often.
+    Then with the random.choice function, the popular hours have a higher likelihood
+    of getting selected.
+    """
+    weighted_choices = [hour for hour in range(0, 24)]
+    hour_options = []
+    for hour in range(0, 24):
+        for weight in range(get_hour_weight(hour)):
+            hour_options.append(hour)
+    return random.choice(hour_options)
+
+
+def _test_randomness():
+    """
+    A simple test to make sure we're getting real random values (simply
+    by the look of the numbers :) )
+    Get a random hour for 10,000 times, we expect to mostly get numbers
+    between 12 and 14, which is supposed to be like real "store" data
+    """
+    stats = {}
+    for i in xrange(10000):
+        random_hour = get_random_hour()
+        if random_hour in stats:
+            stats[random_hour] += 1
+        else:
+            stats[random_hour] = 1
+
 
 def customer_dates_generator():
     """
@@ -15,7 +59,8 @@ def customer_dates_generator():
         random.randint(2010, 2015),
         random.randint(1, 12),
         random.randint(1, 28),
-        random.randint(0, 23),
+        # random.randint(0, 23),
+        get_random_hour(),
         random.randint(0, 59)
     )
 
