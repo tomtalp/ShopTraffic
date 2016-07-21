e7432dd70ab92c0366a6f4ca86356db095b87eb6
Tom <tomtalp93@gmail.com>
2016-07-21 18:29:25 +0300

    Add device-rank sql query

--- sql-queries.sql
+++ sql-queries.sql
@@ -17,6 +17,16 @@ select avg(datediff(minute, first_seen, last_seen))
 from atom_wifi_data
 ;
 -- Get the most popular hours
-select to_char(first_seen, 'HH24'), count(to_char(first_seen, 'HH24')) from atom_wifi_data
+select to_char(first_seen, 'HH24') as hour, count(to_char(first_seen, 'HH24')) from atom_wifi_data
 group by to_char(first_seen, 'HH24')
+order by hour
 ;
+select *
+from (
+  select device_type, count(device_type) as counter, rank() over (order by count(device_type) desc) as counter_rank
+  from atom_wifi_data
+  where device_type is not null
+  group by device_type
+)
+where counter_rank <= 10
+order by counter_rank asc
